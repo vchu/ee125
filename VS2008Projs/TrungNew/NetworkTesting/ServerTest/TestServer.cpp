2b5125be09c5d3d6f7a28b5c96ccd876089b97cc
trungnob@gmail.com <trungnob@gmail.com@c7e5bc68-916c-11dd-99c3-0167725eca9f>
2008-11-09 19:49:53 +0000

    Test Svn stuff

--- VS2008Projs/TrungNew/NetworkTesting/ServerTest/TestServer.cpp
+++ VS2008Projs/TrungNew/NetworkTesting/ServerTest/TestServer.cpp
@@ -10,7 +10,7 @@ unsigned __stdcall Answer(void* a) {
     std::string r = s->ReceiveLine();
     if (r.empty()) break;
 	std::cout<<"Client sent:"<<r<<"\n";
-	
+	//blah
   
    // s->SendLine(r);
   }
