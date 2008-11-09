f3097cc5136a1ae0f35d19223ee37b78fda7305b
trungnob@gmail.com <trungnob@gmail.com@c7e5bc68-916c-11dd-99c3-0167725eca9f>
2008-11-09 19:28:40 +0000

    ServerTest ClientTest

---
+++ trunk/VS2008Projs/TrungNew/NetworkTesting/ServerTest/TestServer.cpp
@@ -0,0 +1,35 @@
+#include "_Socket.h"
+#include <process.h>
+#include <string>
+#include <iostream>
+
+unsigned __stdcall Answer(void* a) {
+  Socket* s = (Socket*) a;
+
+  while (1) {
+    std::string r = s->ReceiveLine();
+    if (r.empty()) break;
+	std::cout<<"Client sent:"<<r<<"\n";
+	
+  
+   // s->SendLine(r);
+  }
+
+  delete s;
+
+  return 0;
+}
+
+int main(int argc, char* argv[]) {
+   std::cout<< "Initializing server at port 2000";
+  SocketServer in(2000,5);
+
+  while (1) {
+    Socket* s=in.Accept();
+
+    unsigned ret;
+    _beginthreadex(0,0,Answer,(void*) s,0,&ret);
+  }
+ 
+  return 0;
+}
